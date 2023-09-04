f2a6c00b42aa93f0b0c155563cb12f31c8051873
Gabriel Campos <gahbrihell@outlook.com>
2023-09-04 12:31:22 -0300

    SECOOOOOND, public folder

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const port = 4000
 app.use(express.json()) // irá fazer o parse de arquivos JSON
 //Rotas de conteudo público
 app.use('/', express.static('public'))
+//configura o favicon
+app.use('/favicon.ico', express.static('public/images/computador.png'))
+
 //Rotas da API
 app.get('/api', (req, res) => {
     res.status(200).json({
@@ -14,6 +17,17 @@ app.get('/api', (req, res) => {
     })
 
 })
+//rotas de exceção - deve ser a ultima!!!
+app.use(function (req, res){
+    res.status(404).json({
+        errors: [{
+            value: `${req.originalUrl}`,
+            msg: `A rota ${req.originalUrl} não existe nesta API!`,
+            param: 'Invalid route'
+        }]
+    })
+})
+
 app.listen(port, function(){
 console.log(`Servidor rodando na porta ${port}`)
 })
